19359cce37eb7795904d5233f45c5a5166631ce4
sagarpal04 <125257733+sagarpal04@users.noreply.github.com>
2024-10-20 17:25:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ const Header = () => (
   <nav className="flex justify-between flex-wrap p-2 md:p-4 max-w-[1800px] mx-auto">
     <div className="flex items-center gap-6">
       <Logo />
-      <h2 className="uppercase text-2xl font-bold">Neux Bank</h2>
+      <h2 className="uppercase text-2xl font-bold">Nexus Bank</h2>
     </div>
     <div className="items-center gap-10 hidden md:flex">
       <a
